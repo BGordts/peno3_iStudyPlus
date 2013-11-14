dc27178ecdb31d1258f4ec142dfaa8d551456a40
BGordts <borisgordts@hotmail.com>
2013-11-14 13:44:08 +0100

    Vergeten om templates toe te voegen

---
+++ app/forms/loginForm.py
@@ -0,0 +1,8 @@
+from flask_wtf import Form
+from wtforms import TextField
+from wtforms.validators import DataRequired
+from app import app
+
+class LoginForm(Form):
+    email = TextField('email', validators=[DataRequired()])
+    password = TextField('password', validators=[DataRequired()])
