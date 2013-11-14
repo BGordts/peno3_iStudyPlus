22e16865404fcc0c719bbcb207d4ea195fd7ccb7
BGordts <borisgordts@hotmail.com>
2013-11-14 15:05:35 +0100

    Login werkt half :)

--- app/controllers/userController.py
+++ app/controllers/userController.py
@@ -4,6 +4,7 @@ from functools import wraps
 from app import app
 from app import db
 from app.models.user import User
+from werkzeug._internal import _log
 from app.forms.loginForm import LoginForm
 
 def login_required(test):
@@ -13,7 +14,7 @@ def login_required(test):
             return test(*args, **kwargs)
         else:
             flash('you need to login first.')
-            return redirect(url_for('login.html'))
+            return redirect(url_for('login'))
     return wrap
 
 @app.route('/home')
@@ -29,11 +30,12 @@ def welcome():
 def login():
     error = None    
     if request.method == 'POST':
-        if not isValidLogin(request.form['username'],request.form['password']):
+        if not request.form["username"] == "admin" and False:
+        #if not isValidLogin(request.form['username'],request.form['password']):
             error = 'invalid username or password, please try again.'
         else:
-            session['user'] = User.query.filter_by(username= username).first()
-            return redirect(url_for('welcome'))
+            #session['user'] = User.query.filter_by(email= username).first()
+            return redirect(url_for("welcome"))
     return render_template('pages/login.html' , error = error)
    
 
