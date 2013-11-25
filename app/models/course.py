04872af02c12d136deb9fe1c6daa25be5c514f1f
samlanduydt <sam.landuydt@student.kuleuven.be>
2013-11-25 16:21:56 +0100

    bleble

---
+++ app/models/course.py
@@ -0,0 +1,28 @@
+from flask import *
+
+from app import app
+from app import db
+
+from app.models.session import Session
+
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    course = db.Column(db.String(120), unique=True)
+    
+    def __init__(self, course):
+        self.course = course
+    
+    def getAllUser(self, course):
+        pass
+    
+    @staticmethod
+    def getAllCourses():
+        courses= []
+        for course in Course.query.all():
+            courses.append(course.course)
+        return courses
+        
+        
+    
+    
