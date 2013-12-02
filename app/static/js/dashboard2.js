4a31d1ab8e745a44810c230294f741b89ed0b7a1
samlanduydt <sam.landuydt@student.kuleuven.be>
2013-12-02 13:58:36 +0100

    aa

--- app/static/js/dashboard2.js
+++ app/static/js/dashboard2.js
@@ -15,8 +15,140 @@ controller('appCtrl', function ($scope) {
     $scope.panelState = "view";
     $scope.sessionType = "live";
     $scope.activityType = "study";
-    $scope.courselist = [{'data': [1,2,3,4]}, [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []];
-    $scope.sessionlist = [{data: [1,2,3,4]}, [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []];
+    $scope.courselist = [{
+        "data1": [
+            {
+                x: 0.2,
+                y: 0.6
+            },
+            {
+                x: 0.3,
+                y: 0.6
+            },
+            {
+                x: 0.4,
+                y: 0.6
+            },
+    ],
+        "data2": [
+            {
+                x: 0.2,
+                y: 0.3
+            },
+            {
+                x: 0.3,
+                y: 0.10
+            },
+            {
+                x: 0.4,
+                y: 0.12
+            },
+    ]
+    }, {
+        "data1": [
+            {
+                x: 0.2,
+                y: 0.2
+            },
+            {
+                x: 0.3,
+                y: 0.4
+            },
+            {
+                x: 0.4,
+                y: 0.8
+            },
+    ],
+        "data2": [
+            {
+                x: 0.2,
+                y: 0.5
+            },
+            {
+                x: 0.3,
+                y: 0.9
+            },
+            {
+                x: 0.4,
+                y: 0.12
+            },
+    ]
+    }, {
+        "data1": [
+            {
+                x: 0.2,
+                y: 0.2
+            },
+            {
+                x: 0.3,
+                y: 0.6
+            },
+            {
+                x: 0.4,
+                y: 0.6
+            },
+    ],
+        "data2": [
+            {
+                x: 0.2,
+                y: 0.5
+            },
+            {
+                x: 0.3,
+                y: 0.10
+            },
+            {
+                x: 0.4,
+                y: 0.12
+            },
+    ]
+    }];
+    $scope.sessionlist = [{
+        "data": [
+            {
+                x: 0.2,
+                y: 0.5
+            },
+            {
+                x: 0.3,
+                y: 0.10
+            },
+            {
+                x: 0.4,
+                y: 0.12
+            },
+    ]
+    }, {
+        "data": [
+            {
+                x: 0.2,
+                y: 0.5
+            },
+            {
+                x: 0.3,
+                y: 0.10
+            },
+            {
+                x: 0.4,
+                y: 0.12
+            },
+    ]
+    }, {
+        "data": [
+            {
+                x: 0.2,
+                y: 0.5
+            },
+            {
+                x: 0.3,
+                y: 0.10
+            },
+            {
+                x: 0.4,
+                y: 0.12
+            },
+    ]
+    }];
     /*alert('zever');*/
     $scope.name = "You";
     $scope.commonStudents = [{
@@ -186,13 +318,34 @@ directive('dashboardPanel', function ($scope) {
         restrict: 'EA',
         replace: true,
         templateUrl: "dashboard_course-list.tpl",
-        link: function () {
-
-            },
-        controller: function ($scope) {
-            $scope.chartdata = $scope.item.data;
-            console.log($scope.chartdata)
+        link: function (scope, element, attrs) {
+            scope.$watch('selectedItem', function (newVal, oldVal) {
+                if (typeof nevVal === "undefined" && newVal == null) {
+                    console.log("hey fa" + newVal + " " + oldVal);
+                    //scope.chartdata = scope.item.data2;
+                } else {
+                    console.log("jmjklm")
+                    scope.chartdata = scope.item["data" + newVal.id];
+                }
+            });
         },
+        controller: function ($scope) {
+            $scope.chartdata = $scope.item.data1;
+            console.log($scope.chartdata)
+
+            $scope.items = [
+                {
+                    id: 1,
+                    name: 'Foo'
+                },
+                {
+                    id: 2,
+                    name: 'Bar'
+                }
+               ];
+
+            $scope.selectedItem = null;
+        }
     }
 })
 
@@ -201,7 +354,7 @@ directive('dashboardPanel', function ($scope) {
         restrict: 'EA',
         replace: true,
         templateUrl: "dashboard_session-list.tpl",
-        link: function () {
+        link: function (scope, element, attrs) {
 
         },
         controller: function ($scope) {
@@ -211,219 +364,187 @@ directive('dashboardPanel', function ($scope) {
     }
 })
 
-/*.controller('mainCtrl', function AppCtrl($scope) {
-    $scope.options = {
-        width: 500,
-        height: 300,
-        'bar': 'aaa'
-    };
-    $scope.data = [1, 2, 3, 4];
-    console.log(data)
-    $scope.hovered = function (d) {
-        $scope.barValue = d;
-        $scope.$apply();
-    };
-    $scope.barValue = 'None'; 
-}) */
-    .directive('dynamicGraph', function () {
-        var chart = d3.custom.barChart();
-        return {
-            restrict: 'E',
-            replace: true,
-            template: '<div class="chart"></div>',
-            scope: {
-                height: '=height',
-                data: '=',
-                hovered: '&hovered'
-            },
-            link: function (scope, element, attrs) {
-              /*  console.log(attrs.data) */
-                var array = JSON.parse("[" + attrs.data + "]")
-                var chartEl = d3.select(element[0]);
-                chart.on('customHover', function (d, i) {
-                    scope.hovered({
-                        args: d
-                    });
-                });
-/* chartEl.datum([1,2,3,4]).call(chart) */
-                scope.$watch(array, function (newVal, oldVal) {
-                    
-                    chartEl.datum(newVal).call(chart);
-                });
+//.controller('mainCtrl', function AppCtrl ($scope) {
+//            $scope.options = {width: 500, height: 300, 'bar': 'aaa'};
+//            $scope.data = [1, 2, 3, 4];
+//            $scope.hovered = function(d){
+//                $scope.barValue = d;
+//                $scope.$apply();
+//            };
+//            $scope.barValue = 'None';
+//        })
+.directive('dynamicGraph', function () {
+    var integer = 0;
+    return {
+        restrict: 'E',
+        replace: true,
+        template: '<div class="chart"></div>',
+        scope: {
+            chartdata: '=',
+        },
+        link: function (scope, element, attrs) {
+            d3.custom = {};
+            d3.custom.barChart = function module() {
+                var margin = {
+                    top: 20,
+                    right: 20,
+                    bottom: 40,
+                    left: 40
+                },
+                    width = 500,
+                    height = 500,
+                    gap = 0,
+                    ease = 'cubic-in-out';
+                var svg, duration = 500;
 
-                scope.$watch('height', function (d, i) {
-                    chartEl.call(chart.height(scope.height));
-                })
-            }
-        }
-    })
-/*    .directive('chartForm', function () {
-        return {
-            restrict: 'E',
-            replace: true,
-            controller: function AppCtrl($scope) {
-                $scope.update = function (d, i) {
-                    $scope.data = randomData();
-                };
+                var dispatch = d3.dispatch('customHover');
 
-                function randomData() {
-                    return d3.range(~~(Math.random() * 50) + 1).map(function (d, i) {
-                        return~~ (Math.random() * 1000);
+                function exports(_selection) {
+                    _selection.each(function (_data) {
+
+                        var chartW = width - margin.left - margin.right,
+                            chartH = height - margin.top - margin.bottom;
+                        var x = d3.scale.linear()
+                            .domain([0, d3.max(_data, function (d) {
+                                return (d.x);
+                            })])
+                            .range([0, chartW]);
+
+                        var y = d3.scale.linear()
+                            .domain([0, d3.max(_data, function (d) {
+                                return (d.y);
+                            })])
+                            .range([chartH, 0]);
+
+                        var x1 = d3.scale.linear()
+                            .domain([0, d3.max(_data, function (d) {
+                                return (d.x);
+                            })])
+                            .range([0, chartW]);
+
+                        var y1 = d3.scale.linear()
+                            .domain([0, d3.max(_data, function (d) {
+                                return (d.y);
+                            })])
+                            .range([chartH, 0]);
+
+                        var xAxis = d3.svg.axis()
+                            .scale(x)
+                            .orient('bottom');
+
+                        var yAxis = d3.svg.axis()
+                            .scale(y)
+                            .orient('left');
+
+                        var line = d3.svg.line()
+                            .defined(function (d) {
+                                return d.y != null;
+                            })
+                            .x(function (d) {
+                                return x(d.x);
+                            })
+                            .y(function (d) {
+                                return y(d.y);
+                            });
+
+
+                        if (!svg) {
+                            svg = d3.select(this)
+                                .append('svg')
+                                .classed('chart', true);
+
+                            var container = svg.append('g').classed('container-group', true);
+                            container.append('g').classed('chart-group', true);
+                            container.append('g').classed('x-axis-group axis', true);
+                            container.append('g').classed('y-axis-group axis', true);
+                        }
+
+                        svg.transition().duration(duration).attr({width: width,height: height})
+                        svg.select('.container-group')
+                        .attr({transform: 'translate(' + margin.left + ',' + margin.top + ')'});
+
+                        svg.select('.x-axis-group.axis')
+                            .transition()
+                            .duration(duration)
+                            .ease(ease)
+                            .attr({
+                                transform: 'translate(0,' + (chartH) + ')'
+                            })
+                            .call(xAxis);
+
+                        svg.select('.y-axis-group.axis')
+                            .transition()
+                            .duration(duration)
+                            .ease(ease)
+                            .call(yAxis);
+                       svg.append("path")
+                            .attr("class", "line")
+                            .attr("d", line); 
+
+                        /*                        var bars = svg.select('.chart-group')
+                            .selectAll('.bar')
+                            .data(_data);
+
+                        bars.enter().append('rect')
+                            .classed('bar', true)
+                            .attr("class", "dot")
+                            .attr("cx", line.x())
+                            .attr("cy", line.x())
+                            .attr("r", 3.5)
+                            .on('mouseover', dispatch.customHover); */
+                        var bars = svg.select('.chart-group')
+                            .selectAll('.bar')
+                            .data(_data);
+                        bars.enter().append("circle")
+                            .classed('bar', true)
+                            .attr("class", "dot")
+                            .attr("cx", line.x())
+                            .attr("cy", line.y())
+                            .attr("r", 3.5)
+                            .on('mouseover', dispatch.customHover);
+                        bars.transition()
+                            .duration(duration)
+                            .ease(ease);
+                        bars.exit().transition().style({opacity: 0}).remove();
+                        bars.exit().transition().selectAll('cirlce').remove();
+
+                        duration = 500;
+
                     });
                 }
-            },
-            template: '<div class="form">' +
-                'Height: {{options.height}}<br />' +
-                '<input type="range" ng-model="options.height" min="100" max="800"/>' +
-                '<br /><button ng-click="update()">Update Data</button>' +
-                '<br />Hovered bar data: {{barValue}}</div>'
+                exports.width = function (_x) {
+                    if (!arguments.length) return width;
+                    width = parseInt(_x);
+                    return this;
+                };
+                exports.height = function (_x) {
+                    if (!arguments.length) return height;
+                    height = parseInt(_x);
+                    duration = 0;
+                    return this;
+                };
+                exports.gap = function (_x) {
+                    if (!arguments.length) return gap;
+                    gap = _x;
+                    return this;
+                };
+                exports.ease = function (_x) {
+                    if (!arguments.length) return ease;
+                    ease = _x;
+                    return this;
+                };
+                d3.rebind(exports, dispatch, 'on');
+                return exports;
+            };
+
+            var chart = d3.custom.barChart();
+            var chartEl = d3.select(element[0]);
+
+            scope.$watch('chartdata', function (newVal, oldVal) {
+                chartEl.datum(newVal).call(chart);
+                integer = integer + 1;
+                console.log(integer)
+            });
         }
-    });*/
-              
-
-d3.custom = {};
-
-d3.custom.barChart = function module() {
-    var margin = {
-        top: 20,
-        right: 20,
-        bottom: 40,
-        left: 40
-    },
-        width = 500,
-        height = 500,
-        gap = 0,
-        ease = 'cubic-in-out';
-    var svg, duration = 500;
-
-    var dispatch = d3.dispatch('customHover');
-
-    function exports(_selection) {
-        _selection.each(function (_data) {
-            
-
-            var chartW = width - margin.left - margin.right,
-                chartH = height - margin.top - margin.bottom;
-
-            var x1 = d3.scale.ordinal()
-                .domain(_data.map(function (d, i) {
-                    return i;
-                }))
-                .rangeRoundBands([0, chartW], .1);
-
-            var y1 = d3.scale.linear()
-                .domain([0, d3.max(_data, function (d, i) {
-                    return d;
-                })])
-                .range([chartH, 0]);
-
-            var xAxis = d3.svg.axis()
-                .scale(x1)
-                .orient('bottom');
-
-            var yAxis = d3.svg.axis()
-                .scale(y1)
-                .orient('left');
-
-            var barW = chartW / _data.length;
-
-            if (!svg) {
-                svg = d3.select(this)
-                    .append('svg')
-                    .classed('chart', true);
-
-                var container = svg.append('g').classed('container-group', true);
-                container.append('g').classed('chart-group', true);
-                container.append('g').classed('x-axis-group axis', true);
-                container.append('g').classed('y-axis-group axis', true);
-            }
-
-            svg.transition().duration(duration).attr({
-                width: width,
-                height: height
-            })
-            svg.select('.container-group')
-                .attr({
-                    transform: 'translate(' + margin.left + ',' + margin.top + ')'
-                });
-
-            svg.select('.x-axis-group.axis')
-                .transition()
-                .duration(duration)
-                .ease(ease)
-                .attr({
-                    transform: 'translate(0,' + (chartH) + ')'
-                })
-                .call(xAxis);
-
-            svg.select('.y-axis-group.axis')
-                .transition()
-                .duration(duration)
-                .ease(ease)
-                .call(yAxis);
-
-            var gapSize = x1.rangeBand() / 100 * gap;
-            var barW = x1.rangeBand() - gapSize;
-            var bars = svg.select('.chart-group')
-                .selectAll('.bar')
-                .data(_data);
-            bars.enter().append('rect')
-                .classed('bar', true)
-                .attr({
-                    x: chartW,
-                    width: barW,
-                    y: function (d, i) {
-                        return y1(d);
-                    },
-                    height: function (d, i) {
-                        return chartH - y1(d);
-                    }
-                })
-                .on('mouseover', dispatch.customHover);
-            bars.transition()
-                .duration(duration)
-                .ease(ease)
-                .attr({
-                    width: barW,
-                    x: function (d, i) {
-                        return x1(i) + gapSize / 2;
-                    },
-                    y: function (d, i) {
-                        return y1(d);
-                    },
-                    height: function (d, i) {
-                        return chartH - y1(d);
-                    }
-                });
-            bars.exit().transition().style({
-                opacity: 0
-            }).remove();
-
-            duration = 500;
-
-        });
     }
-    exports.width = function (_x) {
-        if (!arguments.length) return width;
-        width = parseInt(_x);
-        return this;
-    };
-    exports.height = function (_x) {
-        if (!arguments.length) return height;
-        height = parseInt(_x);
-        duration = 0;
-        return this;
-    };
-    exports.gap = function (_x) {
-        if (!arguments.length) return gap;
-        gap = _x;
-        return this;
-    };
-    exports.ease = function (_x) {
-        if (!arguments.length) return ease;
-        ease = _x;
-        return this;
-    };
-    d3.rebind(exports, dispatch, 'on');
-    return exports;
-};
+})
