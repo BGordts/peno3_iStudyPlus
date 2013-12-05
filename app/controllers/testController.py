73948b02414fc83f68a8e9b0673d6924f23d06e6
samlanduydt <sam.landuydt@student.kuleuven.be>
2013-12-05 21:01:17 +0100

    a

--- app/controllers/testController.py
+++ app/controllers/testController.py
@@ -15,6 +15,17 @@ import random
 
 from werkzeug._internal import _log
 
+@app.route('/test/simulateData')
+def simulatedataBase():
+    clearDataBase()
+    data =""
+    data = data + createTestUsers()
+    data = data + createTestSessions()
+    data = data + createUntrackedSession()
+    data = data + createTestSessions()
+    data = data + createUntrackedSession()
+    return data    
+    
 @app.route('/test/clearAllData')
 def clearDataBase():
     db.drop_all()
@@ -35,14 +46,8 @@ def createTestUsers():
         i = i+1
     db.session.commit()
     return data
-
-@app.route('/test/recreateAll')
-def recreateAll():
-    clearDataBase()
-    createTestUsers()
-    return createTestSessions()
  
-@app.route('/test/createRandomTestSessions')   
+@app.route('/test/createTestSessions')   
 def createTestSessions():
     x = 0
     data = ""
@@ -67,7 +72,7 @@ def createTestSessions():
                 pauze.append( (i,i+pauzeTime) )
                 userSession.pauses = json.dumps(pauze)
             generateTestData(userSession,i)
-            i = i+3
+            i = i+50
         userSession.setFeedback(random.random())
         userSession.commitSession()
         data = data + user.email.__str__() + " heeft een tracked session gedaan voor het vak: " + course.course + ". totale studietijd: " + userSession.getSessionDuration().__str__() + " seconden."
@@ -84,19 +89,27 @@ def generateTestData(usersession,timestamp):
     db.session.add(Sensordata("focus", usersession, bool(random.getrandbits(1)), t))
     db.session.commit()
 
-@app.route('/test/untrackedSession')
+@app.route('/test/createUntrackedSession')
 def createUntrackedSession():
-    description = "test untracked session"
-    courseID = 1
-    course = Course.query.get(7)
-    feedback_text = "hopelijk is dit inorde"
-    start_date = datetime(2000,1,1,0,00,00)
-    end_date =  datetime(2000,1,1,12,00,00)
-    user = User.getUserFromSession()
-    usession = UserSession(user, course , description, feedback_text, start_date, end_date)
-    db.session.add(usession)
-    db.session.commit()
-    return "ok"
+    x=0
+    data =""
+    while x <2:
+        userID = random.randint(1,21)
+        user = User.query.get(userID)
+        courseID = random.randint(1,11)
+        course = Course.query.get(courseID)
+        randomStart = random.randint(1386264114,1416264114)
+        start_date = datetime.fromtimestamp(randomStart)
+        randomEnd = randomStart + random.randint(900,14400)
+        end_date = datetime.fromtimestamp(randomEnd)
+        feedback_text = "dat was een lastige studeersessie"
+        userSession = UserSession(user, course , "een untracked studeersessie", feedback_text, start_date, end_date)
+        db.session.add(userSession)
+        db.session.commit()
+        data = data + user.email.__str__() + " heeft een untracked session gedaan voor het vak: " + course.course + ". totale studietijd: " + userSession.getSessionDuration().__str__() + " seconden."
+        data = data + "\n"
+        x=x+1 
+    return data
 
 @app.route('/test/modifieSession')
 def modifieUntrackedSession():
@@ -106,8 +119,8 @@ def modifieUntrackedSession():
     newFeedBack_score = uSession.feedback_score
     newStart_date = uSession.start_date
     newEnd_date = uSession.end_date
-    if not(newDescription == "is deze aangepast?"):
-        newDescription = "is deze aangepast?"
+    if not(newDescription == "description is aangepast"):
+        newDescription = "description is aangepast"
     if not(newFeedBack_score == "nieuweFeedback"):
         newFeedBack_score = "nieuweFeedback"
     if not(newStart_date == newStart_date):
