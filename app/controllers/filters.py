87b5537266d6fa448c51de437dda08dcead6691f
BGordts <borisgordts@hotmail.com>
2013-12-10 11:14:31 +0100

    Forgot to add

---
+++ app/controllers/filters.py
@@ -0,0 +1,42 @@
+from flask import *
+from functools import wraps
+
+def session_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'sessionID' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('need to create a session first')
+            return redirect(url_for('create'))
+    return wrap
+
+def endSession_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if not 'sessionID' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('you need to end session first')
+            return redirect(url_for('tracking'))
+    return wrap
+
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'userID' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('you need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
+def logout_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if not 'userID' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('you need to logout first.')
+            return redirect(url_for('home'))
+    return wrap
